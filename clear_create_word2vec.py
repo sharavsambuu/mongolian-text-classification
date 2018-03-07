f18858b8d75c6e917a7ed83c46fccfa302605520
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2018-03-07 17:16:31 +0800

    creation of word2vec from all the corpuses

--- clear_create_word2vec.py
+++ clear_create_word2vec.py
@@ -1,19 +1,33 @@
 from clear_text_to_array import *
 from gensim.models import Word2Vec
+import glob, json
 
 # корпусыг ачаалах
+'''
 filename = 'corpuses/ikon-news01.txt'
 file = open(filename, 'rt')
 text = file.read()
 file.close()
+'''
 
-sentences = clear_text_to_array(text)
+all_corpuses = ""
 
+print("reading all corpuses, please wait for a little while...")
+for filename in glob.iglob('corpuses/**/*.txt', recursive=True):
+    with open(filename, 'r') as f:
+        json_content = json.load(f)
+        all_corpuses = all_corpuses + " " +json_content['title'] + " \n "+json_content["body"]+" \n "
+print("reading is done.")
+
+print("converting to the sentence array...")
+sentences = clear_text_to_array(all_corpuses)
+print('done.')
+
+print("starting to create word2vec...")
 model = Word2Vec(sentences, min_count=1)
 model.save('model.bin')
 print('word2vec model is saved as gensim file format.')
 #words = list(model.wv.vocab)
 #print(words)
 #print(model['дээд'])
-
-import pdb; pdb.set_trace()
+#import pdb; pdb.set_trace()
