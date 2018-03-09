bab69efb91ac5a63f4379882816d45073dbb214f
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2018-03-09 11:41:09 +0800

    created Singleton for stemmer in order to prevent multiple call

--- clear_text_to_array.py
+++ clear_text_to_array.py
@@ -3,11 +3,11 @@ from nltk.tokenize import word_tokenize
 from nltk import stem
 import string
 from mongolianstopwords import *
-from stemmer_rules import *
+from stemmer import *
 
 def clear_text_to_array(input_text):
     text_sentences = sent_tokenize(input_text)
-    stemmer = stem.RegexpStemmer(stemmer_rules_tuple, min=6)
+    stemmer = Stemmer.instance().stemmer
     sentences = []
     for text_sentence in text_sentences:
         # өгүүлбэрийн текстийг үгүүд болгож хувиргах
