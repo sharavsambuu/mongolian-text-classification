03b30a2d305efd168ca95d5718432950966b688e
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2018-03-11 00:24:33 +0800

    Custom user for django app

--- djangoapp/app/models.py
+++ djangoapp/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
-# Create your models here.
+class CustomUserManager(UserManager):
+    pass
+
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
--- djangoapp/djangoapp/settings.py
+++ djangoapp/djangoapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# custom settings
+AUTH_USER_MODEL = 'app.CustomUser'
