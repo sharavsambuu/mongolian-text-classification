c7b583572245b3046acd9db462aadec0ebb5d34f
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2018-03-11 10:00:39 +0800

    some prediction tests on corpuses from gogo.mn

--- use_freezed_model.py
+++ use_freezed_model.py
@@ -27,30 +27,53 @@ prediction_op     = graph.get_tensor_by_name("prediction_op:0")
 
 dataset_helper = DataSetHelper()
 
-with open('./corpuses_test/world_news_gogo_mn.txt', 'r') as content_file:
-    content = content_file.read()
 
-max_seq_length = 500
-num_classes    = 6
-
-word_ids = dataset_helper.sentence_to_ids(content, max_seq_length)
-
-x_batch = []
-for i in range(24):
-    x_batch.append(word_ids)
-
-x_batch = np.array(x_batch)
-
-print("----- X BATCH ----")
-print(x_batch)
-results = []
+def get_class_name(x):
+    switcher = {
+        0: lambda: "economy"   ,
+        1: lambda: "health"    ,
+        2: lambda: "politics"  ,
+        3: lambda: "society"   ,
+        4: lambda: "technology",
+        5: lambda: "world"      
+    }
+    return switcher.get(x, lambda: "UNKNOWN")()
 
 sess = tf.Session(graph=graph)
-results_tf = sess.run(prediction_op, feed_dict={input_placeholder: x_batch})
-for i in results_tf:
-    softmax_result = softmax(i)
-    results.append(softmax_result)
 
-print("----- RESULT -----")
-print(results)
+def predict_class(sess, filename):
+    with open(filename, 'r') as content_file:
+        content = content_file.read()
+
+    max_seq_length = 500
+    num_classes = 6
+
+    word_ids = dataset_helper.sentence_to_ids(content, max_seq_length)
+    x_batch = []
+    for i in range(24):
+        x_batch.append(word_ids)
+    x_batch = np.array(x_batch)
+    results = []
+
+    results_tf = sess.run(prediction_op, feed_dict={input_placeholder: x_batch})
+    for i in results_tf:
+        softmax_result = softmax(i)
+        argmax = softmax_result.argmax(axis=0)
+        name = get_class_name(argmax)
+        results.append([softmax_result, argmax, name])
+
+    print("result: ", results[0][2])
+
+print("trying to predict world news")
+predict_class(sess, "./corpuses_test/world_news_gogo_mn.txt")
+
+print("trying to predict economy news")
+predict_class(sess, "./corpuses_test/economy_news_gogo_mn.txt")
+
+print("trying to predict technology news")
+predict_class(sess, "./corpuses_test/technology_news_gogo_mn.txt")
+
+print("trying to predict health news")
+predict_class(sess, "./corpuses_test/health_news_gogo_mn.txt")
+
 sess.close()
