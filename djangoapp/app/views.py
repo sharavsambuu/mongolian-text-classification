2c7e099be72296529d53cf8e9f60e6f8d98cb02d
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2018-03-12 05:47:41 +0800

    using freezed model with django and RPC

--- djangoapp/app/views.py
+++ djangoapp/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import MongolianTextForm
+import xmlrpc.client
 
 def index(request):
     form = MongolianTextForm()
@@ -12,7 +13,9 @@ def classify(request):
         form = MongolianTextForm(request.POST)
         if form.is_valid():
             content = form.cleaned_data['content']
-            return render(request, 'classify.html', {'content' : content})
+            with xmlrpc.client.ServerProxy("http://localhost:50001/") as proxy:
+                news_class = proxy.predict_class_from_text(content)
+            return render(request, 'classify.html', {'content' : content, 'news_class': news_class})
         return redirect('index')
     else:
         return redirect('index')
